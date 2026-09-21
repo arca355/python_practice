795c39e491ce0aebe87043fc097416ee08a77eca
Arkadiy <cenenovn@gmail.com>
2026-09-21 16:57:36 +0300

    Создан чекер надежности паролей и файл с паролями

---
+++ password_chekcer/chekcer.py
@@ -0,0 +1,28 @@
+def check_file(filename):
+    result = {}
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            password = line.strip()
+            problems = check_strenght(password)
+            result[password] = problems if problems else ["Нет проблем"]
+    return result
+
+def check_strenght(password):
+    problems = []
+    special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~"
+    if len(password) < 8:
+        problems.append("Пароль должен иметь более 8 символов. ")
+    if not any(c.isupper() for c in password):
+        problems.append("Пароль должен содержать хотя бы одну заглавную букву")
+    if not any(c in special_characters for c in password):
+        problems.append("Пароль должен содержать хотя бы один специальный символ")
+    if not any(c.isdigit() for c in password):
+        problems.append("Пароль должен содержать хотя бы одну цифру")
+    return problems
+
+def save_report(result,filename):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for password, problems in result.items():
+            file.write(f"Пароль: {password} Проблемы: {', '.join(problems)}\n")
+            
+save_report(check_file('passwords.txt'),'report.txt')
